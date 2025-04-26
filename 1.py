ac6e0b9fdb34973655b91b168353c53f112cca33
t3nq <50250208+t3nq@users.noreply.github.com>
2025-04-26 16:42:33 +0300

    Update 1.py
    Добавление генератора случайных чисел

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
+from random import randint
+from time import sleep
 
+
+a = int(input("Введите число от: "))
+b = int(input("Введите число до: "))
+
+result = randint(a, b)
+
+print(f'Подбираем число в диапозоне от {a} до {b}...')
+sleep(2)
+print(f"Случайное число: {result}")
