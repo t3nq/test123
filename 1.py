54491e26e29271a8a3ad1a9de566243d543972c3
t3nq <50250208+t3nq@users.noreply.github.com>
2025-04-26 16:54:22 +0300

    Update 1.py
    v1.2

--- 1.py
+++ 1.py
@@ -2,15 +2,24 @@ from random import randint
 from time import sleep
 
 
-def generate(value1, value2):
-    return randint(value1, value2)
+class Randomizer:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+        self.result = self.__get_genereted_value()
+
+    def __get_genereted_value(self):
+        return randint(self.value1, self.value2)
+
+    def get_info(self):
+        return f"Случайное число: {self.result}"
 
 
 a = int(input("Введите число от: "))
 b = int(input("Введите число до: "))
 
-result = generate(a, b)
+rand = Randomizer(a, b)
 
 print(f'Подбираем число в диапозоне от {a} до {b}...')
 sleep(2)
-print(f"Случайное число: {result}")
+print(rand.get_info())
