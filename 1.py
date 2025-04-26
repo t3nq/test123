686f2cabd933dc14c8fd1f6170dae032c4eeda72
t3nq <50250208+t3nq@users.noreply.github.com>
2025-04-26 16:49:24 +0300

    Update 1.py
    v1.1

--- 1.py
+++ 1.py
@@ -2,10 +2,14 @@ from random import randint
 from time import sleep
 
 
+def generate(value1, value2):
+    return randint(value1, value2)
+
+
 a = int(input("Введите число от: "))
 b = int(input("Введите число до: "))
 
-result = randint(a, b)
+result = generate(a, b)
 
 print(f'Подбираем число в диапозоне от {a} до {b}...')
 sleep(2)
